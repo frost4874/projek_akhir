69540e9b3651d2a67086e020c98d30e06e929ea0
Farras Wirawan <farraswirawan4874@gmail.com>
2024-05-16 17:04:13 +0700

    update register flutter

--- app/Http/Controllers/flutter/FlutterRegisterController.php
+++ app/Http/Controllers/flutter/FlutterRegisterController.php
@@ -5,7 +5,8 @@ namespace App\Http\Controllers\flutter;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Log; // Import class Log
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Storage;
 use App\Models\Biodata;
 use App\Models\KecamatanDesa;
 use App\Models\Desa;
@@ -14,10 +15,32 @@ class FlutterRegisterController extends Controller
 {
     public function register(Request $request)
 {
-    // Memasukkan pesan log sebelum validasi data
-    Log::info('Memulai proses registrasi');
-    Log::info('Data yang diterima:', $request->all());
+    $dirktp = "foto_ktp/";
+    $dirkk = "foto_kk/";
+    $imagekk = $request->file('foto_kk');
+    $imagektp = $request->file('foto_ktp');
+    $imgnamektp = $request->input('nik') . '_ktp' . "." . "jpg";
+    $imgnamekk = $request->input('nik') . '_kk' . "." . "jpg";
 
+    if ($request->has('foto_ktp')) {
+        $request->validate([
+            'foto_ktp' => 'image|mimes:jpeg,png,jpg,gif|max:2048',
+        ]);
+        Storage::disk('public')->put($dirktp.$imgnamektp, file_get_contents($imagektp));
+        Log::info('Foto KTP berhasil disimpan: ' . $dirktp.$imgnamektp);
+    }
+
+    if ($request->has('foto_kk')) {
+        $request->validate([
+            'foto_kk' => 'image|mimes:jpeg,png,jpg,gif|max:2048',
+        ]);
+        Storage::disk('public')->put($dirkk.$imgnamekk, file_get_contents($imagekk));
+        Log::info('Foto KK berhasil disimpan: ' . $dirkk.$imgnamekk);
+    }
+
+    Log::info('Data yang diterima:', $request->all());
+    
+    // Validasi data
     $validatedData = $request->validate([
         'nik' => 'required|numeric|unique:biodata',
         'nama' => 'required|string|max:100',
@@ -30,48 +53,21 @@ class FlutterRegisterController extends Controller
         'tgl_lahir' => 'required|date',
         'alamat' => 'nullable|string',
         'password' => 'required|string|min:8',
-        // 'foto_ktp' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048',
-        // 'foto_kk' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048',
     ]);
-
-    // Memasukkan pesan log setelah validasi data
-    Log::info('Data berhasil divalidasi');
-
-    // Simpan gambar KTP
-    if ($request->hasFile('foto_ktp')) {
-        $fotoKtp = $request->file('foto_ktp');
-        $ext = $fotoKtp->getClientOriginalExtension();
-        $nik = $validatedData['nik'];
-        $fotoKtpFileName = $nik . '_ktp.' . $ext;
-        $fotoKtpPath = $fotoKtp->storeAs('public/foto_ktp', $fotoKtpFileName);
     
-        // Memasukkan pesan log setelah penyimpanan gambar KTP
-        Log::info('Gambar KTP berhasil disimpan', ['path' => $fotoKtpPath]);
-    }
-    
-
-    // Simpan gambar KK
-if ($request->hasFile('foto_kk')) {
-    $fotoKk = $request->file('foto_kk');
-    $ext = $fotoKk->getClientOriginalExtension();
-    $nik = $validatedData['nik'];
-    $fotoKkFileName = $nik . '_kk.' . $ext;
-    $fotoKkPath = $fotoKk->storeAs('public/foto_kk', $fotoKkFileName);
-
-    // Memasukkan pesan log setelah penyimpanan gambar KK
-    Log::info('Gambar KK berhasil disimpan', ['path' => $fotoKkPath]);
-}
-
-
     // Ambil nama kecamatan dan desa dari formulir
     $kecamatanNama = $validatedData['kecamatan'];
     $desaNama = $validatedData['desa'];
-
+    
     // Ambil kecamatan dan desa dari database
     $kecamatan = KecamatanDesa::where('nama', $kecamatanNama)->first();
     $desa = Desa::where('nama', $desaNama)->first();
 
     if ($kecamatan && $desa) {
+        // Memasukkan pesan log sebelum menyimpan data
+        Log::info('Memulai proses registrasi');
+        
+        
         $biodata = Biodata::create([
             'nik' => $validatedData['nik'],
             'nama' => $validatedData['nama'],
@@ -84,8 +80,8 @@ if ($request->hasFile('foto_kk')) {
             'tgl_lahir' => $validatedData['tgl_lahir'],
             'alamat' => $validatedData['alamat'],
             'password' => Hash::make($validatedData['password']),
-            'foto_ktp' => $fotoKtpFileName,
-            'foto_kk' => $fotoKkFileName,
+            'foto_ktp' => $imgnamektp ?? null,
+            'foto_kk' => $imgnamekk ?? null,
             'role' => 'Pemohon',
             'status' => 'Tidak Aktif',
         ]);
@@ -100,11 +96,12 @@ if ($request->hasFile('foto_kk')) {
     } else {
         // Memasukkan pesan log jika kecamatan atau desa tidak valid
         Log::info('Kecamatan atau desa tidak valid');
-
+        
         return response()->json([
             'error' => 'Kecamatan atau desa tidak valid'
         ], 400);
     }
 }
+
 
 }
